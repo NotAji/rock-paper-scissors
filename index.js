d7c7bfa5132434cc9e3f081e08ac9adb31118908
NotAji <aimoralita23@gmail.com>
2025-02-24 23:37:07 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let rockHuman = document.getElementById("rockHUman");
+let paperHuman = document.getElementById("paperHuman");
+let scissorsHuman = document.getElementById("scissorsHuman");
+
+let rockCPU = document.getElementById("rockCPU");
+let paperCPU = document.getElementById("paperCPU");
+let scissorsCPU = document.getElementById("scissorsCPU");
+
+let humanResult = document.getElementById("humanResult");
+let cpuResult = document.getElementById("cpuResult");
