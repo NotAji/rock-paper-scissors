b4803ca27bcf690e80f97954c2ecde99c16bc951
NotAji <aimoralita23@gmail.com>
2025-02-25 00:46:37 +0800

    made rock event playable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let rockHuman = document.getElementById("rockHUman");
+let rockHuman = document.getElementById("rockHuman");
 let paperHuman = document.getElementById("paperHuman");
 let scissorsHuman = document.getElementById("scissorsHuman");
 
@@ -7,4 +7,51 @@ let paperCPU = document.getElementById("paperCPU");
 let scissorsCPU = document.getElementById("scissorsCPU");
 
 let humanResult = document.getElementById("humanResult");
-let cpuResult = document.getElementById("cpuResult");
+let cpuResult = document.getElementById("cpuResult"); 
+
+let winner = document.getElementById("winner");
+let roundNum = document.getElementById("roundNum");
+let whoWin = document.getElementById("winner");
+
+let begin = document.getElementById("start");
+let round = 0;
+
+begin.addEventListener('click', () => {
+    round++;
+    roundNum.textContent = round;
+    const min = 1;
+    const max = 3;
+    humanResult.textContent = "Choose quickly";
+    cpuResult.textContent = "Picking...";
+    whoWin.textContent = "Who will win...";
+    begin.style.visibility = "Hidden";
+
+    rockHuman.addEventListener('click', () => {
+        let choice = Math.floor(Math.random() * max) + min;
+        
+        humanResult.textContent = "You picked Rock";
+        
+        switch(choice){
+            case 1:
+                cpuResult.textContent = "CPU picked Rock";
+                winner.textContent = "It's a Draw";
+                begin.style.visibility = "visible";
+                break;
+
+            case 2:
+                cpuResult.textContent = "CPU picked Paper";
+                winner.textContent = "CPU wins!!";
+                begin.style.visibility = "visible";
+                break;
+
+            case 3:
+                cpuResult.textContent = "CPU picked Scissors";
+                winner.textContent = "Player wins!!";
+                begin.style.visibility = "visible";
+                break;
+        }
+    });
+    if(round == 6){
+        exit();
+    }
+})
